5f0652077358319eb347f439a0e59caf2250d90b
mmcho626 <office_hagi@yahoo.co.jp>
2019-09-18 15:27:42 +0000

    ヘッダー変更(form_for,コントローラへの実装)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'books/new'
+  get 'books/create'
+  get 'books/index'
+  get 'books/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root :to => 'home#top'
