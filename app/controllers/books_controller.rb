047ba81d64db71a14df10820e76495515eb61f35
mmcho626 <office_hagi@yahoo.co.jp>
2019-09-19 05:55:02 +0000

    削除機能先に追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,14 @@ end
 
 
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+end
+
+
+
 
 private
     def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
 
-  get 'books/new'
-  get 'books/create'
-  get 'books/index'
-  get 'books/show'
+  # get 'books/new'
+  # get 'books/create'
+  # get 'books/index'
+  # get 'books/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root :to => 'home#top'
 
 devise_for :users
-  resources :books, only: [:new, :create, :index, :show]
+  # resources :books, only: [:new, :create, :index, :show, :destroy]
+
+
+
+resources :books, only: [:new, :create, :index, :show, :destroy]
+
+
+
 
 end
