61ed3a91557e49d9a5016d78e101bd91f0213bf3
mmcho626 <office_hagi@yahoo.co.jp>
2019-09-19 11:37:06 +0000

    現在ログインしているユーザーをsessionで取得する処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,16 @@ class ApplicationController < ActionController::Base
 
 
 
+
+
+before_action :current_user
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+
+
+
 end
