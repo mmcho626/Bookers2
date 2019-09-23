f42f229a5cf198699dd5690cbb3a4c5ff8715c17
mmcho626 <office_hagi@yahoo.co.jp>
2019-09-23 06:38:52 +0000

    db:migrate エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # get 'books/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root :to => 'home#top'
+root :to => 'top#top'
 
 devise_for :users
   # resources :books, only: [:new, :create, :index, :show, :destroy]
--- db/migrate/20190922123240_book_images.rb
+++
@@ -1,5 +0,0 @@
-class BookImages < ActiveRecord::Migration[5.2]
-  def change
-  	drop_table :book_images
-  end
-end
